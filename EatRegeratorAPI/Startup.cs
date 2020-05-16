9eaee61f84e7582e90aaa7e83a5f57be264f9c75
alexei619@gmail.com <tokmakov251198>
2020-05-16 16:01:13 +0300

    Добавлен swagger

--- EatRegeratorAPI/Startup.cs
+++ EatRegeratorAPI/Startup.cs
@@ -29,11 +29,23 @@ namespace EatRegeratorAPI
       services.AddControllers().AddNewtonsoftJson(options =>
           options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
           );
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "EatRegeratorApi", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EatRegeratorApi V1");
+        c.RoutePrefix = string.Empty;
+      });
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
